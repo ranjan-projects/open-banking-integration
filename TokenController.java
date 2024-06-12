55e9ea5ac33a4fc5a532414656f95330918dcd37
ranjan-projects <88734043+ranjan-projects@users.noreply.github.com>
2024-06-12 14:41:51 +0100

    Update TokenController.java

--- TokenController.java
+++ TokenController.java
@@ -1,15 +1,18 @@
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TokenController {
 
     @Autowired
-    private WebClient webClient;
+    private RestTemplate restTemplate;
 
     @PostMapping("/getToken")
     public String getToken() {
@@ -17,16 +20,17 @@ public class TokenController {
         String clientSecret = "YOUR_CLIENT_SECRET";
         String url = "https://as1.obie.uk.ozoneapi.io/token";
 
-        String response = webClient.post()
-                .uri(url)
-                .headers(headers -> headers.setBasicAuth(clientId, clientSecret))
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters.fromFormData("grant_type", "client_credentials")
-                        .with("scope", "accounts openid"))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(clientId, clientSecret);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "client_credentials");
+        body.add("scope", "accounts openid");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+        String response = restTemplate.postForObject(url, request, String.class);
         return response;
     }
 }
